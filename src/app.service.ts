99f7a0e46140359e69ff7337005c943138f27cdd
Leonardo Torres <leonardo.torres@airfox.com>
2020-12-10 22:38:51 -0300

    corrigindo codigo

--- src/app.service.ts
+++ src/app.service.ts
@@ -12,7 +12,7 @@ import { Sequelize } from 'sequelize-typescript';
 @Injectable()
 export class AppService {
   getHello(): string{
-    return 'Hello World';
+    return "ola";
   }
   constructor(private sequelize: Sequelize) {}
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCzMQFBv-mVGTilORO5wj49l_2axV8E_lQ",
+    authDomain: "gamificacao-8b961.firebaseapp.com",
+    databaseURL: "https://gamificacao-8b961.firebaseio.com",
+    projectId: "gamificacao-8b961",
+    storageBucket: "gamificacao-8b961.appspot.com",
+    messagingSenderId: "619942269141",
+    appId: "1:619942269141:web:466dc750aa9907a1c2d25c"
+};
